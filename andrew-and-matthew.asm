3fc8bd4ceda1cad74e8f7f2e2e9b0e7593b50188
kingmomus <52150100+kingmomus@users.noreply.github.com>
2020-02-12 21:45:18 -0800

    Cleaned up lcd.inc and organized fsm code
    Disabled the external interupt. We need to figure it out.

--- andrew-and-matthew.asm
+++ andrew-and-matthew.asm
@@ -60,9 +60,6 @@ TIMER0_RATE   	EQU 100000     ; 2048Hz squarewave (peak amplitude of CEM-1203 sp
 TIMER0_RELOAD 	EQU ((65536-(CLK/(2*TIMER0_RATE))))
 TEMP_THRESHOLD 	EQU 500
 
-TIMER1_RATE   EQU 200     ; 200Hz, for a timer tick of 5ms
-TIMER1_RELOAD EQU ((65536-(CLK/(2*TIMER1_RATE))))
-
 FLASH_CE    EQU P2.4
 SOUND       EQU P2.7
 
@@ -85,10 +82,15 @@ BTN_START   EQU 1 ; TODO: assign port
 BTN_SETVAL  EQU 1
 BTN_INCR    EQU 1
 BTN_DECR    EQU 1
-FLASH_CE    EQU P2.4
-SOUND       EQU P2.7
 
-cseg
+; These 'equ' must match the wiring between the microcontroller and the LCD!
+LCD_RS EQU P0.5
+LCD_RW EQU P0.6
+LCD_E  EQU P0.7
+LCD_D4 EQU P1.2
+LCD_D5 EQU P1.3
+LCD_D6 EQU P1.4
+LCD_D7 EQU P1.6
 
 org 0x0000 ; Reset vector
     ljmp MainProgram
@@ -111,32 +113,7 @@ org 0x0023 ; Serial port receive/transmit interrupt vector (not used in this cod
 org 0x005b ; CCU interrupt vector.  Used in this code to replay the wave file.
 	ljmp CCU_ISR
 
-cseg
-; These 'equ' must match the wiring between the microcontroller and the LCD!
-LCD_RS equ P0.5
-LCD_RW equ P0.6
-LCD_E  equ P0.7
-LCD_D4 equ P1.2
-LCD_D5 equ P3.1
-LCD_D6 equ P1.4
-LCD_D7 equ P1.6
-
-message0: db 'Current stage: Idle',0
-message1: db 'Current stage: Ramp to soak',0
-message2: db 'Current stage: Preheat/soak',0
-message3: db 'Current stage: Ramp to peak',0
-message4: db 'Current stage: Heating at peak',0
-message5: db 'Current stage: Cooling Down',0
-temp_message: db 'temperature is: ',0
-Time_message: db 'Duration:',0
-
-$NOLIST
-$include(LCD_4bit_LPC9351.inc) ; A library of LCD related functions and utility macros
-$include(lcd_4bit.inc)
-$include(speaker.inc)
-$LIST
-
-DSEG at 0x30
+dseg at 0x30
 x:   ds 4
 y:   ds 4
 bcd: ds 5
@@ -146,6 +123,7 @@ Var_temp:   ds 2
 Var_sec:    ds 2
 Var_power:  ds 1
 Val_to_set: ds 1
+temp: 		ds 1
 
 SoundINDEX: 		ds 1 ; Index of the sound
 
@@ -155,12 +133,31 @@ Temp_peak:  ds 2 ; 2
 Time_peak:  ds 2 ; 3
 Temp_cool:  ds 2 ; 4
 
-BSEG
+bseg
 mf: dbit 1
 emergency_shutoff: dbit 1
+Change_flag: dbit 1
+
+cseg
+
+message0: db 'Current stage: Idle',0
+message1: db 'Current stage: Ramp to soak',0
+message2: db 'Current stage: Preheat/soak',0
+message3: db 'Current stage: Ramp to peak',0
+message4: db 'Current stage: Heating at peak',0
+message5: db 'Current stage: Cooling Down',0
+temp_message: db 'temperature is: ',0
+Time_message: db 'Duration:', 0
+message_intro: db 'frick!', 0
+
+Line1: db 'CH3 CH2 CH1 CH0', 0
+Line2: db 'xxx xxx xxx xxx', 0
 
 $NOLIST
-$INCLUDE(math32.inc)
+$include(LCD_4bit_LPC9351.inc) ; A library of LCD related functions and utility macros
+$include(lcd_4bit.inc)
+$include(speaker.inc)
+$include(math32.inc)
 $LIST
 
 ; interrupt stuff here
@@ -169,7 +166,7 @@ emergency_ISR_Init:
 	ret
 
 emergency_ISR:
-	cpl p1.7
+	cpl P1.7
 	cpl emergency_shutoff
 	Wait_Milli_Seconds(#150)
 	reti
@@ -357,7 +354,7 @@ InitADC0:
 	; Setup ADC0
 	setb BURST0 ; Autoscan continuos conversion mode
 	mov	ADMODB,#0x20 ;ADC0 clock is 7.3728MHz/2
-	mov	ADINS,#0x04 ; Select the AD0DAT2 of ADC0 for conversion
+	mov	ADINS,#0x0f ; Select the four channels of ADC0 for conversion
 	mov	ADCON0,#0x05 ; Enable the converter and start immediately
 	; Wait for first conversion to complete
 InitADC0_L1:
@@ -533,9 +530,6 @@ CRC16_TL:
 	db	01Fh, 03Eh, 05Dh, 07Ch, 09Bh, 0BAh, 0D9h, 0F8h
 	db	017h, 036h, 055h, 074h, 093h, 0B2h, 0D1h, 0F0h
 
-Line1: db 'CH3 CH2 CH1 CH0', 0
-Line2: db 'xxx xxx xxx xxx', 0
-
 ; Display a 3-digit BCD number in the LCD
 LCD_3BCD:
 	mov a, bcd+1
@@ -564,13 +558,15 @@ Wait10us:
 Average_AD0DAT2:
 	Load_x(0)
 	mov R5, #100
-	Sum_loop0:
+Sum_loop0:
 	mov y+3, #0
 	mov y+2, #0
 	mov y+1, #0
-	mov y+0, AD0DAT2
+	mov y+0, AD0DAT0
+
 	lcall add32
 	lcall Wait10us
+
 	djnz R5, Sum_loop0
 	Load_y(100)
 	lcall div32
@@ -599,157 +595,157 @@ Display_ADC_Values:
 	Wait_Milli_Seconds(#250)
 	ret
 
-; What's this?????
+; ; What's this?????
 
-Incr_value:
-    mov a, Val_to_set
-    cjne a, #0, IV1
-    mov a, Temp_soak
-    ChangeDisplay(Temp_soak)
-    cjne a, #255, IV0a
-    ret
-IV0a:
-    add a, #0x01
-    da a
-    mov Temp_soak, a
-    ret
-IV1:
-    cjne a, #1, IV2
-    mov a, Time_soak
-    ChangeDisplay(Time_soak)
-    cjne a, #255, IV1a
-    ret
-IV1a:
-    add a, #0x01
-    da a
-    mov Time_soak, a
-    ret
-IV2:
-    cjne a, #2, IV3
-    mov a, Temp_peak
-    ChangeDisplay(Temp_peak)
-    cjne a, #255, IV2a
-    ret
-IV2a:
-    add a, #0x01
-    da a
-    mov Temp_peak, a
-    ret
-IV3:
-    cjne a, #3, IV4
-    mov a, Time_peak
-    ChangeDisplay(Time_peak)
-    cjne a, #255, IV3a
-    ret
-IV3a:
-    add a, #0x01
-    da a
-    mov Time_peak, a
-    ret
-IV4:
-    cjne a, #4, IV5
-    mov a, Temp_cool
-    ChangeDisplay(Temp_cool)
-    cjne a, #255, IV4a
-    ret
-IV4a:
-    add a, #0x01
-    da a
-    mov Temp_cool, a
-    ret
-IV5:
-    ret
+; Incr_value:
+;     mov a, Val_to_set
+;     cjne a, #0, IV1
+;     mov a, Temp_soak
+;     ;ChangeDisplay(Temp_soak)
+;     cjne a, #255, IV0a
+;     ret
+; IV0a:
+;     add a, #0x01
+;     da a
+;     mov Temp_soak, a
+;     ret
+; IV1:
+;     cjne a, #1, IV2
+;     mov a, Time_soak
+;     ;ChangeDisplay(Time_soak)
+;     cjne a, #255, IV1a
+;     ret
+; IV1a:
+;     add a, #0x01
+;     da a
+;     mov Time_soak, a
+;     ret
+; IV2:
+;     cjne a, #2, IV3
+;     mov a, Temp_peak
+;     ;ChangeDisplay(Temp_peak)
+;     cjne a, #255, IV2a
+;     ret
+; IV2a:
+;     add a, #0x01
+;     da a
+;     mov Temp_peak, a
+;     ret
+; IV3:
+;     cjne a, #3, IV4
+;     mov a, Time_peak
+;     ;ChangeDisplay(Time_peak)
+;     cjne a, #255, IV3a
+;     ret
+; IV3a:
+;     add a, #0x01
+;     da a
+;     mov Time_peak, a
+;     ret
+; IV4:
+;     cjne a, #4, IV5
+;     mov a, Temp_cool
+;     ;ChangeDisplay(Temp_cool)
+;     cjne a, #255, IV4a
+;     ret
+; IV4a:
+;     add a, #0x01
+;     da a
+;     mov Temp_cool, a
+;     ret
+; IV5:
+;     ret
 
-Decr_value:
-    mov a, Val_to_set
-    cjne a, #0, DV1
-    mov a, Temp_soak
-    ChangeDisplay(Temp_soak)
-    cjne a, #0, DV0a
-    ret
-DV0a:
-    subb a, #0x01
-    da a
-    mov Temp_soak, a
-    ret
-DV1:
-    cjne a, #1, DV2
-    mov a, Time_soak
-    ChangeDisplay(Time_soak)
-    cjne a, #0, DV1a
-    ret
-DV1a:
-    subb a, #0x01
-    da a
-    mov Time_soak, a
-    ret
-DV2:
-    cjne a, #2, DV3
-    mov a, Temp_peak
-    ChangeDisplay(Temp_peak)
-    cjne a, #0, DV2a
-    ret
-DV2a:
-    subb a, #0x01
-    da a
-    mov Temp_peak, a
-    ret
-DV3:
-    cjne a, #3, DV4
-    mov a, Time_peak
-    ChangeDisplay(Time_peak)
-    cjne a, #0, DV3a
-    ret
-DV3a:
-    subb a, #0x01
-    da a
-    mov Time_peak, a
-    ret
-DV4:
-    cjne a, #4, DV5
-    mov a, Temp_cool
-    ChangeDisplay(Temp_cool)
-    cjne a, #0, DV4a
-    ret
-DV4a:
-    subb a, #0x01
-    da a
-    mov Temp_cool, a
-    ret
-DV5:
-    ret
+; Decr_value:
+;     mov a, Val_to_set
+;     cjne a, #0, DV1
+;     mov a, Temp_soak
+;     ;ChangeDisplay(Temp_soak)
+;     cjne a, #0, DV0a
+;     ret
+; DV0a:
+;     subb a, #0x01
+;     da a
+;     mov Temp_soak, a
+;     ret
+; DV1:
+;     cjne a, #1, DV2
+;     mov a, Time_soak
+;     ;ChangeDisplay(Time_soak)
+;     cjne a, #0, DV1a
+;     ret
+; DV1a:
+;     subb a, #0x01
+;     da a
+;     mov Time_soak, a
+;     ret
+; DV2:
+;     cjne a, #2, DV3
+;     mov a, Temp_peak
+;     ;ChangeDisplay(Temp_peak)
+;     cjne a, #0, DV2a
+;     ret
+; DV2a:
+;     subb a, #0x01
+;     da a
+;     mov Temp_peak, a
+;     ret
+; DV3:
+;     cjne a, #3, DV4
+;     mov a, Time_peak
+;     ;ChangeDisplay(Time_peak)
+;     cjne a, #0, DV3a
+;     ret
+; DV3a:
+;     subb a, #0x01
+;     da a
+;     mov Time_peak, a
+;     ret
+; DV4:
+;     cjne a, #4, DV5
+;     mov a, Temp_cool
+;     ;ChangeDisplay(Temp_cool)
+;     cjne a, #0, DV4a
+;     ret
+; DV4a:
+;     subb a, #0x01
+;     da a
+;     mov Temp_cool, a
+;     ret
+; DV5:
+;     ret
 
-Set_Reflow_Params:
-    ; If SETVAL button is pressed, change variable to be incremented
-	jb BTN_SETVAL, SRP1
-	Wait_Milli_Seconds(#50)
-	jb BTN_SETVAL, SRP1
-	jnb BTN_SETVAL, $
-    mov a, Val_to_set
-    cjne a, #4, SV1
-    mov Val_to_set, #0x00 ; Set Val_to_set to 0 if it's at 4
-    sjmp SRP1
-SV1:
-    mov a, Val_to_set ; Otherwise increment Val_to_set by 1
-    add a, #0x01
-    da a
-    mov Val_to_set, a
-SRP1:
-    ; if INCR is pressed, add one to current variable (maximum of 255)
-    jb BTN_SETVAL, SRP2
-	Wait_Milli_Seconds(#100)
-	jb BTN_SETVAL, SRP2
-	jnb BTN_SETVAL, $
-    lcall Incr_value
-SRP2:
-    ; if DECR is pressed, subtract one from current variable (minimum of 0)
-    jb BTN_SETVAL, SRP3
-	Wait_Milli_Seconds(#100)
-	jb BTN_SETVAL, SRP3
-	jnb BTN_SETVAL, $
-    lcall Decr_Value
-SRP3:
-    ret
+; Set_Reflow_Params:
+;     ; If SETVAL button is pressed, change variable to be incremented
+; 	jb BTN_SETVAL, SRP1
+; 	Wait_Milli_Seconds(#50)
+; 	jb BTN_SETVAL, SRP1
+; 	jnb BTN_SETVAL, $
+;     mov a, Val_to_set
+;     cjne a, #4, SV1
+;     mov Val_to_set, #0x00 ; Set Val_to_set to 0 if it's at 4
+;     sjmp SRP1
+; SV1:
+;     mov a, Val_to_set ; Otherwise increment Val_to_set by 1
+;     add a, #0x01
+;     da a
+;     mov Val_to_set, a
+; SRP1:
+;     ; if INCR is pressed, add one to current variable (maximum of 255)
+;     jb BTN_SETVAL, SRP2
+; 	Wait_Milli_Seconds(#100)
+; 	jb BTN_SETVAL, SRP2
+; 	jnb BTN_SETVAL, $
+;     lcall Incr_value
+; SRP2:
+;     ; if DECR is pressed, subtract one from current variable (minimum of 0)
+;     jb BTN_SETVAL, SRP3
+; 	Wait_Milli_Seconds(#100)
+; 	jb BTN_SETVAL, SRP3
+; 	jnb BTN_SETVAL, $
+;     lcall Decr_Value
+; SRP3:
+;     ret
 
 ;---------------------------------;
 ; Main program. Includes hardware ;
@@ -760,17 +756,15 @@ MainProgram:
     mov SP, #0x7F
     
     lcall Ports_Init ; Default all pins as bidirectional I/O. See Table 42.
-    lcall InitADC0 ; Call after 'Ports_Init'
+    lcall LCD_4BIT
+    lcall Double_Clk
+	lcall InitSerialPort
+	lcall InitADC0 ; Call after 'Ports_Init'
 	lcall InitDAC1 ; Call after 'Ports_Init'
 	lcall CCU_Init
 	lcall Init_SPI
-	lcall Timer0_Init ;enable timer0 interrupt
 
-	lcall LCD_4BIT
-    lcall Double_Clk
-	lcall InitSerialPort
-
- 	lcall emergency_ISR_Init ;enable pin12 emergency stop
+ 	;lcall emergency_ISR_Init ;enable pin12 emergency stop
 	
 	clr TMOD20 	; Stop CCU timer
 	setb EA ; Enable global interrupts.
@@ -786,7 +780,12 @@ MainProgram:
     Send_Constant_String(#Line2)
 	
 forever_loop:
-	
+	lcall Display_ADC_Values
+
+	cpl P2.6
+
+	Wait_Milli_Seconds(#250)
+
 	ljmp forever_loop
 	
 END
